01c984644644ffe387cbdb6ee38a365c2946b6f8
kensuke suzuki <35k.0101@gmail.com>
2021-05-09 16:47:10 +0900

    devise実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+  
   root 'events#index'
   resources :events do
     collection do
       get :chart
     end    
-  end  
+  end
+
 end
